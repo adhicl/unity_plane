e46b904156a9c993ede2b924951c6176e66a70ac
AdhiCL <adhicl@gmail.com>
2024-02-07 08:44:16 +0700

    add opening

---
+++ Assets/Scripts/LoadNextScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextScene : MonoBehaviour
+{
+	public string newScene;
+
+	private void OnEnable()
+	{
+		SceneManager.LoadScene(newScene, LoadSceneMode.Single);
+	}
+}
---
+++ Assets/Scripts/TItleScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TItleScene : MonoBehaviour
+{
+	[SerializeField] private PlayableDirector playableDirector;
+	[SerializeField] private GameObject titleScene;
+
+	public void OnStart()
+	{
+		titleScene.SetActive(false);
+		playableDirector.Play();
+	}
+}
